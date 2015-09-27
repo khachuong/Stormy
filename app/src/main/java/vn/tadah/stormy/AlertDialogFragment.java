1cf3f275620290d99e723aff75779768ca1ef8b3
Kha Chuong <khachuongvn@hotmail.com>
2015-09-27 18:59:50 +0700

    Initial Commit

--- app/src/main/java/vn/tadah/stormy/AlertDialogFragment.java
+++ app/src/main/java/vn/tadah/stormy/AlertDialogFragment.java
@@ -1,9 +1,24 @@
 package vn.tadah.stormy;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
 
 /**
  * Created by Khachuong on 9/27/15.
  */
 public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button), null);
+        AlertDialog dialog = builder.create();
+        return dialog;
+
+    }
 }
--- app/src/main/java/vn/tadah/stormy/MainActivity.java
+++ app/src/main/java/vn/tadah/stormy/MainActivity.java
@@ -1,37 +1,103 @@
 package vn.tadah.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    public static  final  String TAG =  MainActivity.class.getSimpleName();
+    private  CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        String apiKey = "47bf69c694f654d7f03a4778aa2751d0";
+        double latitude = 21.0333;
+        double longitude = 105.8500;
+        String forcecastURL = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+        if(isNetworkAvailable()){
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(forcecastURL).build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+                }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if(response.isSuccessful()){
+                            mCurrentWeather = getCurrentDetails(jsonData);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                        }else{
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: " , e);
+                    }catch (JSONException e){
+                        Log.e(TAG, "Exception caught: " , e);
+                    }
+                }
+            });
+        }else{
+
+            Toast.makeText(MainActivity.this, getString(R.string.network_is_unavailable), Toast.LENGTH_SHORT).show();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+        JSONObject forecast = new JSONObject(jsonData);
+        String timeZone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timeZone);
+
+        JSONObject currently = forecast.getJSONObject("currently");
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timeZone);
+        Log.d(TAG, currentWeather.getFormattedTime());
+        return currentWeather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(info != null && info.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+
     }
 }
